7a4c663da3185395e1e2019eb6ff7668cc78b33c
Sofiane <sofiane.teron@umontreal.ca>
2023-05-22 23:35:08 -0400

    Trying to reverse the lerp

--- Assets/ObstacleMovementManager.cs
+++ Assets/ObstacleMovementManager.cs
@@ -8,7 +8,8 @@ public class ObstacleMovementManager : MonoBehaviour
     public Vector3 RightPos;
     public Vector3 UpPos;
     public Vector3 DownPos;
-    Vector3 CurrentPos; 
+    Vector3 CurrentPos;
+    public Vector3 InitialPos; 
     Material ObjectMaterial;  
     float desiredDuration = 100f;
     float timeElapsed; 
@@ -19,6 +20,7 @@ public class ObstacleMovementManager : MonoBehaviour
     void Start()
     {
         CurrentPos = transform.position;
+        //InitialPos = new Vector3(3.09f,1.75f,0.06f);
         ObjectMaterial = GetComponent<MeshRenderer>().material;
         
     }
@@ -32,14 +34,22 @@ public class ObstacleMovementManager : MonoBehaviour
 
    void ObstacleMovement()
    {
+   // bool isLeft = false;
 
-    if(Input.GetKey(KeyCode.R) && cameraController.IsCamera3D == true && obstacleSelectionManager.isSelected == true)
+    if(Input.GetKey(KeyCode.R) && cameraController.IsCamera3D == true && obstacleSelectionManager.isSelected == true   )
     {
         Debug.Log("Move");
+        //isLeft = true; 
         timeElapsed += Time.deltaTime; 
         CurrentPos = Vector3.Lerp(CurrentPos,LeftPos,timeElapsed/desiredDuration);
-        transform.position = CurrentPos; 
-    }
+        transform.position = CurrentPos;
+         
+    } else if(Input.GetKey(KeyCode.T) && cameraController.IsCamera3D == true && obstacleSelectionManager.isSelected == true ){
+        //isLeft = false;
+        //Vector3 InitialPos = new Vector3 (3.09f,1.75f,0.06f);
+        CurrentPos = Vector3.Lerp(LeftPos,InitialPos,timeElapsed/desiredDuration);
+        transform.position = CurrentPos;
+    } 
 
  
    }
